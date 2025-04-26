02fda6a64ec8eb7016e7dfb74ba7edecd20fd30c
hellolima <gabrielladelimadev@gmail.com>
2025-04-26 13:57:42 -0300

    feat: receitas

--- api/src/receitas/dto/create-receita.dto.ts
+++ api/src/receitas/dto/create-receita.dto.ts
@@ -1,1 +1,19 @@
-export class CreateReceitaDto {}
+import { ReceitaTipo } from '@prisma/client';
+import { IsNotEmpty, IsNumber, IsEnum, IsDateString } from 'class-validator';
+
+export class CreateReceitaDto {
+  @IsNotEmpty()
+  descricao: string;
+
+  @IsNumber()
+  valor: number;
+
+  @IsEnum(ReceitaTipo)
+  tipo: ReceitaTipo;
+
+  @IsDateString()
+  data: Date;
+
+  @IsNumber()
+  usuarioId: number;
+}
--- api/src/receitas/receitas.controller.ts
+++ api/src/receitas/receitas.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ReceitasService } from './receitas.service';
 import { CreateReceitaDto } from './dto/create-receita.dto';
 import { UpdateReceitaDto } from './dto/update-receita.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { User } from '../auth/usuario.decorator';
 
 @Controller('receitas')
 export class ReceitasController {
   constructor(private readonly receitasService: ReceitasService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createReceitaDto: CreateReceitaDto) {
-    return this.receitasService.create(createReceitaDto);
+  create(@User() user: any, @Body() createReceitaDto: CreateReceitaDto) {
+    return this.receitasService.create(user.id, createReceitaDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.receitasService.findAll();
+  findAll(@User() user: any) {
+    return this.receitasService.findAll(user.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.receitasService.findOne(+id);
+  findOne(@User() user: any, @Param('id') id: string) {
+    return this.receitasService.findOne(user.id, +id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReceitaDto: UpdateReceitaDto) {
-    return this.receitasService.update(+id, updateReceitaDto);
+  update(@User() user: any, @Param('id') id: string, @Body() updateReceitaDto: UpdateReceitaDto) {
+    return this.receitasService.update(user.id, +id, updateReceitaDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.receitasService.remove(+id);
+  remove(@User() user: any, @Param('id') id: string) {
+    return this.receitasService.delete(user.id, +id);
   }
 }
