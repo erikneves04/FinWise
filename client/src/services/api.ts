0aeab239f71c15cf4d6f595f694d0803e4cf555e
Erik Neves <erikrrn04@gmail.com>
2025-04-27 11:23:22 -0300

    change api url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,11 +4,18 @@ import Constants from "expo-constants";
 
 const { expoConfig } = Constants;
 
-const uri = `http://${expoConfig?.hostUri!.split(":").shift()}:3000`; 
+// const uri = `http://${expoConfig?.hostUri!.split(":").shift()}:3000`; 
+const uri = 'https://finwise-production-79cf.up.railway.app'
 console.log("URI: ", uri);
+
+const fixedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjUsImVtYWlsIjoiZXJpayIsImlhdCI6MTc0NTc2MTczNiwiZXhwIjoxNzQ1ODQ4MTM2fQ.LTLHI2N8raF4_ORejKebnnaiAXwWcDbxIRvEB2A8pTg";
 
 export const api = axios.create({
   baseURL: uri,
   //baseURL: "http://20.121.197.157:2030/api",
   withCredentials: true,
+
+  headers: {
+    Authorization: `Bearer ${fixedToken}`, // Adicionando o token no header
+  },
 });
